2db332ab31e6aa55dd485484686253eff60b7af6
Tomasz Truderung <ttruderung@gmail.com>
2025-02-26 16:58:31 +0100

    upate tie_resolution_cli.py

--- py/tie_resolution_cli.py
+++ py/tie_resolution_cli.py
@@ -1,15 +1,19 @@
-import unittest
+import sys
 
 from tie_resolution import tie_resolution
 
-class TestTieResolution(unittest.TestCase):
+systemSeed = sys.argv[1]
+userSeed = sys.argv[2]
+ballotId = sys.argv[3]
+listId = sys.argv[4]
+tiedCandidates = sys.argv[5]
 
-    def test_tie_resolution_against_a_fixture(self):
-        resolution = tie_resolution(
-            s_seed = "systemSeed",
-            u_seed = "userSeed",
-            ballot_id = "ballot-A",
-            list_id = "list-23",
-            tied_candidates = 20,
-        )
-        self.assertEqual([12, 4, 14, 5, 2, 3, 8, 10, 18, 11, 15, 0, 9, 19, 13, 16, 7, 17, 6, 1], resolution)
+resolution = tie_resolution(
+    s_seed = systemSeed,
+    u_seed = userSeed,
+    ballot_id = ballotId,
+    list_id = listId,
+    tied_candidates = int(tiedCandidates),
+)
+
+print(resolution)
